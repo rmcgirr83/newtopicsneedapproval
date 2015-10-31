2179f97c4fe36030dc8beb3349906f68ac30fdf8
Richard McGirr <rmcgirr83@gmail.com>
2015-10-31 11:38:03 -0400

    Initial commit

---
+++ event/listener.php
@@ -0,0 +1,71 @@
+<?php
+/**
+*
+* New Topics Need Approval extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 Rich McGirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace rmcgirr83\newtopicsneedapproval\event;
+
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	public function __construct(\phpbb\auth\auth $auth)
+	{
+		$this->auth = $auth;
+	}
+
+	/**
+	* Assign functions defined in this class to event listeners in the core
+	*
+	* @return array
+	* @static
+	* @access public
+	*/
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.permissions'					=> 'add_permission',
+			'core.modify_submit_post_data'		=> 'modify_submit_post_data',
+		);
+	}
+
+	/**
+	* Add administrative permissions to manage forums
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function add_permission($event)
+	{
+		$permissions = $event['permissions'];
+		$permissions['f_topic_approve'] = array('lang' => 'ACL_F_TOPIC_APPROVE', 'cat' => 'misc');
+		$event['permissions'] = $permissions;
+	}
+
+	public function modify_submit_post_data($event)
+	{
+		$data_array = $event['data'];
+		$mode = $event['mode'];
+
+		if ($mode == 'post' && !$this->auth->acl_get('f_topic_approve', $data_array['forum_id']))
+		{
+			$data_array['force_approved_state'] = ITEM_UNAPPROVED;
+		}
+		$event['data'] = $data_array;
+	}
+}
---
+++ migrations/v_100.php
@@ -0,0 +1,44 @@
+<?php
+/**
+*
+* Profile on mouse over extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2015 Rich McGirr (RMcGirr83)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace rmcgirr83\newtopicsneedapproval\migrations;
+
+/**
+* Migration stage 3: Initial permission
+*/
+class v_100 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\gold');
+	}
+
+	/**
+	* Add or update data in the database
+	*
+	* @return array Array of table data
+	* @access public
+	*/
+	public function update_data()
+	{
+		return array(
+			// Add permission
+			array('permission.add', array('f_topic_approve', false)),
+
+			// Set permissions
+			array('permission.permission_set', array('ROLE_FORUM_FULL', 'f_topic_approve')),
+			array('permission.permission_set', array('ROLE_FORUM_LIMITED', 'f_topic_approve')),
+			array('permission.permission_set', array('ROLE_FORUM_LIMITED_POLLS', 'f_topic_approve')),
+			array('permission.permission_set', array('ROLE_FORUM_STANDARD', 'f_topic_approve')),
+			array('permission.permission_set', array('ROLE_FORUM_POLLS', 'f_topic_approve')),
+			array('permission.permission_set', array('ROLE_FORUM_ONQUEUE', 'f_topic_approve', 'role', false)),
+		);
+	}
+}
